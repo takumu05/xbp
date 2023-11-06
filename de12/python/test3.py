71a7e09461082670286c47bbb283ed23f7de1f15
takumu05 <r202301820ez@jindai.jp>
2023-11-07 00:46:59 +0900

    20231107

--- de12/python/test3.py
+++ de12/python/test3.py
@@ -1,3 +1,5 @@
+#プログラミング各完成版
+#Instagram
 import japanize_kivy
 import urllib.parse
 import kivy
@@ -5,82 +7,91 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
-from kivy.uix.filechooser import FileChooserListView  # ファイル選択用のウィジェットを追加
 import webbrowser
-import os  # ファイルパスを操作するために追加
 kivy.require('1.11.1')
 
-class LinkOpenerApp(App):
+class HashTagSearchApp(App):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.links = []
-        self.image_path = None  # アップロードされた画像のファイルパス
+        self.hashtags = []
 
     def build(self):
         self.layout = BoxLayout(orientation='vertical')
-        self.input_text1 = TextInput(hint_text='enter your account name without "@"')
-        self.input_text2 = TextInput(hint_text='enter a hashtag without "#"')
-        self.add_button = Button(text='create')
-        self.add_button.bind(on_press=self.add_link)
-        self.remove_button = Button(text='remove')
-        self.remove_button.bind(on_press=self.remove_link)
-
-        # 画像ファイル選択用のウィジェット
-        self.file_chooser = FileChooserListView()
-
-        # 画像選択ボタン
-        self.select_image_button = Button(text='Select Image')
-        self.select_image_button.bind(on_press=self.select_image)
-
-        self.layout.add_widget(self.input_text1)
-        self.layout.add_widget(self.input_text2)
+        self.input_text = TextInput(hint_text='enter a hashtag without "#"')
+        self.add_button = Button(text='search')
+        self.add_button.bind(on_press=self.search_hashtag)
+        self.layout.add_widget(self.input_text)
         self.layout.add_widget(self.add_button)
-        self.layout.add_widget(self.remove_button)
-        self.layout.add_widget(self.select_image_button)
-        self.layout.add_widget(self.file_chooser)
-
         return self.layout
 
-    def add_link(self, instance):
-        place = self.input_text2.text.strip()
-        if place:
-            link_button = Button(text=f'#{place}')
-            link_button.bind(on_press=lambda x, link=place: self.open_link(link))
-            self.links.append(link_button)
-            self.layout.add_widget(link_button)
-        self.input_text2.text = ''
+   
 
-    def remove_link(self, instance):
-        if self.links:
-            last_link_button = self.links.pop()
-            self.layout.remove_widget(last_link_button)
+    def search_hashtag(self, instance):
+        hashtag = self.input_text.text.strip()
+        if hashtag:
+            self.hashtags.append(hashtag)
+            hashtag = urllib.parse.quote(hashtag)  # キーワードをエンコード
+            search_url = f'https://www.instagram.com/explore/tags/{hashtag}/'
+            webbrowser.open(search_url)
 
-    def select_image(self, instance):
-        # ファイル選択ダイアログを開く
-        self.file_chooser.path = os.path.expanduser('~')  # デフォルトディレクトリをユーザーホームに設定
-        self.file_chooser.bind(on_submit=self.load_image)
+            # ウィンドウを閉じないようにするためのカスタムロジックを追加
+            pass
 
-    def load_image(self, chooser, file_path):
-        # アップロードされた画像を読み込む
-        self.image_path = file_path
-
-    def open_link(self, place):
-        if self.image_path:
-            place = urllib.parse.quote(place)  # キーワードをエンコード
-            if place:
-                # エンコードされたキーワードとアカウント名を使用してURLを生成
-                search_url = f'https://www.google.com/?client=avast-a-2/{place}/{self.image_path}/'
-                webbrowser.open(search_url)
-        else:
-            print("Please select an image before opening a link.")
 
 if __name__ == '__main__':
-    LinkOpenerApp().run()
+    app = HashTagSearchApp()
+    app.run()
+
+#交通検索
+import japanize_kivy
+import kivy
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+import requests
+# 既存のコードの上部に以下のインポート文を追加します
+from kivy.uix.label import Label
+
+class TransportationApp(App):
+    def build(self):
+        layout = BoxLayout(orientation='vertical')
+        self.result_label = Label(text='', size_hint_y=None, height=44)
+        layout.add_widget(self.result_label)
+        return layout
 
 
+kivy.require('1.11.1')
 
+class TransportationApp(App):
+    def build(self):
+        layout = BoxLayout(orientation='vertical')
+        self.destination_input = TextInput(hint_text='目的地を入力')
+        search_button = Button(text='交通情報を検索', on_press=self.search_transportation)
+        self.result_label = Label(text='', size_hint_y=None, height=44)
 
+        layout.add_widget(self.destination_input)
+        layout.add_widget(search_button)
+        layout.add_widget(self.result_label)
+        return layout
 
+    def search_transportation(self, instance):
+        destination = self.destination_input.text.strip()
+        if destination:
+            # ヤフージャパンのAPIキーを設定
+            api_key = 'YOUR_YAHOO_JAPAN_API_KEY'
+
+            # APIエンドポイント
+            api_url = f'https://map.yahooapis.jp/direction/v1/transit?appid={api_key}&from=&to={destination}&format=json'
+
+            # APIリクエストを送信
+            response = requests.get(api_url)
+
+            if response.status_code == 200:
+                # レスポンスを処理し、結果を取得
+                transportation_info = response.json()
+
+#宿検索
 import japanize_kivy
 import urllib.parse
 import kivy
@@ -88,42 +99,57 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
-from kivy.uix.filechooser import FileChooserListView
 import webbrowser
-import os
 
 kivy.require('1.11.1')
 
-
-class LinkOpenerApp(App):
+class HotelSearchApp(App):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.links = []
-        self.image_path = None
 
     def build(self):
         self.layout = BoxLayout(orientation='vertical')
-        self.input_text1 = TextInput(hint_text='enter your account name without "@"')
-        self.input_text2 = TextInput(hint_text='enter a hashtag without "#"')
-        self.add_button = Button(text='create')
-        self.add_button.bind(on_press=self.add_link)
-        self.remove_button = Button(text='remove')
-        self.remove_button.bind(on_press=self.remove_link)
+        self.location_input = TextInput(hint_text='目的地を入力してください')
+        self.check_in_input = TextInput(hint_text='チェックイン日（例: 2023-11-04）')
+        self.check_out_input = TextInput(hint_text='チェックアウト日（例: 2023-11-07）')
+        self.guests_input = TextInput(hint_text='宿泊人数')
+        self.rooms_input = TextInput(hint_text='部屋数')
+        self.search_button = Button(text='検索')
+        self.search_button.bind(on_press=self.search_hotels)
 
-        
-
-        # 画像ファイル選択用のウィジェット
-        self.file_chooser = FileChooserListView()
-
-        # 画像選択ボタン
-        self.select_image_button = Button(text='Select Image')
-        self.select_image_button.bind(on_press=self.select_image)
-
-        self.layout.add_widget(self.input_text1)
-        self.layout.add_widget(self.input_text2)
-        self.layout.add_widget(self.add_button)
-        self.layout.add_widget(self.remove_button)
-        self.layout.add_widget(self.select_image_button)
-        self.layout.add_widget(self.file_chooser)
+        self.layout.add_widget(self.location_input)
+        self.layout.add_widget(self.check_in_input)
+        self.layout.add_widget(self.check_out_input)
+        self.layout.add_widget(self.guests_input)
+        self.layout.add_widget(self.rooms_input)
+        self.layout.add_widget(self.search_button)
 
         return self.layout
+
+    def search_hotels(self, instance):
+        location = self.location_input.text.strip()
+        check_in_date = self.check_in_input.text.strip()
+        check_out_date = self.check_out_input.text.strip()
+        guests = self.guests_input.text.strip()
+        rooms = self.rooms_input.text.strip()
+
+        if location and check_in_date and check_out_date:
+            # 実際の宿泊サイトのURLに合わせて調整
+            # 以下はBooking.comの例
+            base_url = 'https://www.booking.com/search.html'
+            query_parameters = {
+                'ss': location,
+                'checkin_monthday': check_in_date.split('-')[2],
+                'checkin_year_month': f'{check_in_date.split("-")[0]}-{check_in_date.split("-")[1]}',
+                'checkout_monthday': check_out_date.split('-')[2],
+                'checkout_year_month': f'{check_out_date.split("-")[0]}-{check_out_date.split("-")[1]}',
+                'group_adults': guests,
+                'no_rooms': rooms,
+            }
+            query_string = urllib.parse.urlencode(query_parameters)
+            search_url = f'{base_url}?{query_string}'
+            webbrowser.open(search_url)
+
+if __name__ == '__main__':
+    app = HotelSearchApp()
+    app.run()
